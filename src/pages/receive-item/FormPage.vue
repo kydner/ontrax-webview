61e89d1305e95b6dc910ebb6e3eb191a9fb851c5
Rendika <mhd.rendika@gmail.com>
2025-08-01 22:06:01 +0700

    update

--- src/pages/receive-item/FormPage.vue
+++ src/pages/receive-item/FormPage.vue
@@ -1,4 +1,15 @@
 <template>
-  <div></div>
+  <inventory-form-page v-model="form" :meta="metaReceiveItem"></inventory-form-page>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ReceiveItem } from 'src/common/constants/meta.constant'
+import { IMetaListModule } from 'src/common/interfaces/meta.interface'
+import { InventoryDataRequest } from 'src/common/model/inventory.model'
+import { ReceiveItemResponsePage } from 'src/common/model/receive-item.model'
+import InventoryFormPage from 'src/components/lib/InventoryFormPage.vue'
+import { ref } from 'vue'
+
+const metaReceiveItem: IMetaListModule<ReceiveItemResponsePage> = ReceiveItem
+
+const form = ref({} as InventoryDataRequest)
+</script>
