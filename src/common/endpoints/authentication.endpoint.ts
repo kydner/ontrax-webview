0c92c12295cc572d9f71e717eb79cf414383cb5c
Rendika <mhd.rendika@gmail.com>
2025-08-02 10:54:56 +0700

    update

--- src/common/endpoints/authentication.endpoint.ts
+++ src/common/endpoints/authentication.endpoint.ts
@@ -13,7 +13,8 @@ export const useAuthenticationEndpoint = defineEndpoint({
 
   login: (data: LoginDataRequest) => Post<LoginDataResponse, LoginDataRequest>('v3/auth/login', data),
 
-  profile: () => Get<ProfileResponse>('user/profile'),
+  // profile: () => Get<ProfileResponse>('user/profile'),
+  profile: () => Get<ProfileResponse>('v3/auth/profile'),
 
   logout: () => Delete<unknown>('v3/auth/logout'),
 
