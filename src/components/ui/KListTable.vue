537dd6a3d025ad86e8155509b47020eb7cb38af1
Rendika <mhd.rendika@gmail.com>
2025-08-07 22:57:18 +0700

    update

--- src/components/ui/KListTable.vue
+++ src/components/ui/KListTable.vue
@@ -98,7 +98,7 @@ $list-table-content-height: calc(100vh - 120px);
 
 .list-table__content {
   max-height: $list-table-content-height;
-  @apply tw-overflow-y-auto;
+  @apply tw-overflow-y-auto tw-mb-6;
   &:hover {
     overflow: auto;
   }
