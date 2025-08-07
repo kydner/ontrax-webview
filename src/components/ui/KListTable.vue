1859d1135c1b32a9f69181c2733fb558e674dd27
Rendika <mhd.rendika@gmail.com>
2025-08-07 22:50:43 +0700

    update

--- src/components/ui/KListTable.vue
+++ src/components/ui/KListTable.vue
@@ -94,10 +94,10 @@ defineExpose({ scrollContainer }) // optional: expose for parent control
 </script>
 
 <style scoped lang="scss">
-$list-table-content-height: calc(100vh - 200px);
+$list-table-content-height: calc(100vh - 120px);
 
 .list-table__content {
-  // max-height: $list-table-content-height;
+  max-height: $list-table-content-height;
   @apply tw-overflow-y-auto;
   overflow: hidden;
 
