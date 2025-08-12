84a9526572b41fe4dbf550c5103dc731bbdc1bcb
Rendika <mhd.rendika@gmail.com>
2025-08-12 14:27:13 +0700

    update

--- src/components/page/transfer-item/quality-control/ListContent.vue
+++ src/components/page/transfer-item/quality-control/ListContent.vue
@@ -2,7 +2,7 @@
   <div class="tw-flex tw-items-start tw-justify-between tw-cursor-pointer" v-ripple @click.stop="handleClick">
     <div class="tw-basis-6/12">
       <div class="tw-flex tw-flex-col tw-space-y-2">
-        <div class="tw-font-semibold">{{ item?.receiveNumber }}</div>
+        <div class="tw-font-semibold">{{ item?.transferNumber }}</div>
         <div class="tw-flex tw-flex-col tw-space-y-2">
           <div class="tw-flex tw-flex-col tw-space-y-1 tw-basis-auto">
             <div class="tw-flex tw-items-center tw-space-x-2">
