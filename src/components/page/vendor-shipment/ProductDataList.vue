0c4a954442603dd19411316b73bda71ead0989c2
Rendika <mhd.rendika@gmail.com>
2025-08-13 23:30:15 +0700

    update

--- src/components/page/vendor-shipment/ProductDataList.vue
+++ src/components/page/vendor-shipment/ProductDataList.vue
@@ -21,7 +21,6 @@
               v-model="product.qtyReceived"
               :allow-increase="false"
               :max="product.qtyOrdered"
-              :disable="vendorValue.status === 'IN_TRANSIT'"
               @increase="handleIncrease(index)"
               @zero:confirm="handleZeroConfirm(index)"
             />
