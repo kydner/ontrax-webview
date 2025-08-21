f9f8f0b054ef09a6314732248a4330115ee1c19f
Rendika <mhd.rendika@gmail.com>
2025-08-21 15:28:57 +0700

    update

--- src/layouts/AuthLayout.vue
+++ src/layouts/AuthLayout.vue
@@ -1,15 +1,16 @@
 <template>
-  <div>
-    <div class="auth-layout__container">
-      <div class="tw-w-full tw-h-screen tw-overflow-y-auto">
-        <div class="tw-flex tw-flex-col tw-items-center tw-justify-center tw-h-full">
-          <div class="tw-pb-4 md:tw-p-0 tw-text-center tw-mb-4">
-            <div class="tw-font-semibold tw-text-4xl md:tw-text-5xl tw-mb-4">Login</div>
-            <p class="tw-text-sm md:tw-text-base tw-text-secondary-text">Welcome Back, You’ve been Missed !</p>
-          </div>
-          <div class="tw-w-full tw-max-w-[30rem] tw-p-4 md:tw-p-0">
-            <router-view />
-          </div>
+  <div class="auth-layout__container">
+    <div class="tw-w-full tw-h-screen tw-overflow-y-auto">
+      <div class="tw-flex tw-flex-col tw-items-center tw-justify-center tw-h-full">
+        <!-- Title -->
+        <div class="tw-text-center tw-mb-6">
+          <div class="tw-font-semibold tw-text-4xl md:tw-text-5xl tw-mb-3">Login</div>
+          <p class="tw-text-sm md:tw-text-base tw-text-secondary-text">Welcome Back, You’ve been Missed !</p>
+        </div>
+
+        <!-- Form -->
+        <div class="auth-layout__form">
+          <router-view />
         </div>
       </div>
     </div>
@@ -24,5 +25,10 @@
     background: url('~assets/images/base-background.svg');
     @apply tw-content-['-'] tw-absolute tw-bg-no-repeat md:tw-bg-repeat-x tw-bg-cover tw-text-surface tw-inset-0 tw-top-0;
   }
+}
+
+.auth-layout__form {
+  max-width: $max-page-width;
+  @apply tw-w-full tw-p-4 md:tw-p-0;
 }
 </style>
