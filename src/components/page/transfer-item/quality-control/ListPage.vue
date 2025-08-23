28b12b3943905f08c7582225190d2f4b1caf51a7
Rendika <mhd.rendika@gmail.com>
2025-08-23 19:16:07 +0700

    update

--- src/components/page/transfer-item/quality-control/ListPage.vue
+++ src/components/page/transfer-item/quality-control/ListPage.vue
@@ -7,6 +7,17 @@
     @search="handleSearch"
     @item:selected="(value) => handleStatus(value)"
   >
+    <template #other-left:suffix>
+      <k-meta-top-filter
+        v-model="fromWarehouseId"
+        :label="t('warehouseFrom')"
+        icon="img:/icons/upload-box__secondary-text.svg"
+        :meta="metaLocationWarehouse"
+        option-label="warehouseName"
+        option-value="locationWarehouseId"
+        @item:selected="(value) => handleWarehouse(value)"
+      />
+    </template>
   </operational-top-filter>
   <!-- end filtering -->
 
@@ -36,7 +47,7 @@
   <!-- end meta list table -->
 </template>
 <script setup lang="ts">
-import { TransferItem } from 'src/common/constants/meta.constant'
+import { TransferItem, LocationWarehouse } from 'src/common/constants/meta.constant'
 import { IMetaListModule } from 'src/common/interfaces/meta.interface'
 import { TransferItemRequestPage, TransferItemResponsePage } from 'src/common/model/transfer-item.model'
 import OperationalTopFilter, { TsortDirection } from 'src/components/page/operational/OperationalTopFilter.vue'
@@ -44,7 +55,10 @@ import { ComponentPublicInstance, computed, defineAsyncComponent, ref } from 'vu
 import KMetaListTable from 'src/components/ui/KMetaListTable.vue'
 import { TStatus } from 'src/common/enum/operational.enum'
 import { orderBy } from 'lodash'
-import { TBaseSortDirection } from 'src/common/interfaces/response.interface'
+import { id, TBaseSortDirection } from 'src/common/interfaces/response.interface'
+import KMetaTopFilter from 'src/components/ui/KMetaTopFilter.vue'
+import { LocationWarehouseResponsePage } from 'src/common/model/location-warehouse.model'
+import { useI18n } from 'vue-i18n'
 
 interface Props {
   suffixScroll?: string
@@ -66,7 +80,11 @@ type MetaListTableExposed = {
 
 const emit = defineEmits<Emits>()
 
+const { t } = useI18n()
+
 const metaTransferItem: IMetaListModule<TransferItemResponsePage> = TransferItem
+
+const metaLocationWarehouse: IMetaListModule<LocationWarehouseResponsePage> = LocationWarehouse
 
 const ListContentPage = computed(() => {
   return defineAsyncComponent({
@@ -79,8 +97,14 @@ const currentStatus = ref<TStatus | undefined>()
 
 const statuses: TStatus[] = ['QC_SEND']
 
+const fromWarehouseId = ref<id | undefined>()
+
 const payload = ref({
   status: currentStatus.value || statuses,
+  receiveNumber: search.value || undefined,
+  fromWarehouseId: fromWarehouseId.value,
+  sortColumn: 'CREATED_DATE',
+  sortDirection: 'DESC' as TBaseSortDirection,
 } as TransferItemRequestPage)
 
 const metaListTableRef = ref<ComponentPublicInstance<MetaListTableExposed> | null>(null)
@@ -91,6 +115,7 @@ const buildPayload = (extra: Partial<typeof payload.value> = {}) => ({
   page: 1,
   receiveNumber: search.value || undefined,
   status: currentStatus.value || statuses,
+  fromWarehouseId: fromWarehouseId.value,
   sortColumn: 'CREATED_DATE',
   sortDirection: 'DESC' as TBaseSortDirection,
   ...extra,
@@ -106,4 +131,12 @@ const handleStatus = (value?: TStatus) => {
   payload.value = buildPayload()
   metaListTableRef.value?.loadMore(true)
 }
+
+const handleWarehouse = (value?: id) => {
+  fromWarehouseId.value = value
+  payload.value = buildPayload()
+  metaListTableRef.value?.loadMore(true)
+}
+
+payload.value = buildPayload()
 </script>
